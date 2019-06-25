c11e820e4728a3c12635d3f19bbe0e1d7f2c9b35
Brad Brown <brad.brown@rackspace.com>
2019-06-25 16:53:57 -0500

    Enhancement: add mypy hints and checking

--- jiratools/error_logger.py
+++ jiratools/error_logger.py
@@ -1,20 +1,35 @@
 """Error logging for jiratools."""
+from typing import List, Iterable
+
 from jiratools import add_comment, format_as_code_block
-from jiratools.formatting import format_autoupdate_jira_msg, format_as_jira_table
+from jiratools.formatting import (
+    DataArray,
+    format_autoupdate_jira_msg,
+    format_as_jira_table,
+)
+
+DataHeaders = List[str]
 
 
-def add_jira_error_comment(jira_id, error_msg, **format_kwargs):
+class JiraEntry:
+    """Typing mock for Jira Entry data."""
+
+    jira_id: str
+    error_message: str
+
+
+def add_jira_error_comment(jira_id: str, error_msg: str, **format_kwargs) -> str:
     """
     Add a comment to a JIRA with a formatted error message.
 
     Args:
-        jira_id (str): the Issue ID of the JIRA to be updated
-        error_msg (str): the raw error message to include in the comment
+        jira_id: the Issue ID of the JIRA to be updated
+        error_msg: the raw error message to include in the comment
         **format_kwargs: formatting keyword args
             to be passed to jiratools.formatting.format_jira_msg
 
     Returns:
-        str: the Issue ID of the JIRA which received the comment
+        the Issue ID of the JIRA which received the comment
 
     """
     add_comment(
@@ -25,15 +40,19 @@ def add_jira_error_comment(jira_id, error_msg, **format_kwargs):
 
 
 def add_jira_comment_with_table(
-    jira_id, data_headers, data_array, msg_prefix="", **format_kwargs
-):
+    jira_id: str,
+    data_headers: DataHeaders,
+    data_array: DataArray,
+    msg_prefix: str = "",
+    **format_kwargs
+) -> str:
     """
     Add a comment to a JIRA with a formatted data table.
 
     Args:
-        jira_id (str): the Issue ID of the JIRA to be updated
-        data_headers (list of str): a list of header column names
-        data_array (list of list of str,int): An array of lists of strings,
+        jira_id: the Issue ID of the JIRA to be updated
+        data_headers: a list of header column names
+        data_array: An array of lists of strings,
             representing table rows. e.g.::
 
             [["a", "b", "c"], ["d", "e", "f"]]
@@ -54,24 +73,26 @@ def add_jira_comment_with_table(
     return jira_id
 
 
-def update_jira_for_errors(jiras, *errors, **format_kwargs):
+def update_jira_for_errors(
+    jiras: Iterable[JiraEntry], *errors: str, **format_kwargs
+) -> List[str]:
     """
     Auto-Update JIRAs if errors are found that match the jira list.
 
     Args:
-        jiras (list, obj): an iterable of objects
+        jiras: an iterable of objects
             that each contain two required attributes
 
-            - jira_id (str): the Issue Id of the JIRA to be updated
-            - error_message (str): an error message substring which, if found,
+            - jira_id: the Issue Id of the JIRA to be updated
+            - error_message: an error message substring which, if found,
               will trigger and update of the JIRA with the actual error message.
 
-        *errors (str): an error message to be checked against the ``jiras`` for a match
+        *errors: an error message to be checked against the ``jiras`` for a match
         **format_kwargs: formatting keyword args
             to be passed to jiratools.formatting.format_jira_msg
 
     Returns:
-        list(str): A list of JIRA Issue IDs that were updated.
+        A list of JIRA Issue IDs that were updated.
 
     """
     jiras_commented = []
